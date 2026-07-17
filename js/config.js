5fc5703d19d257557cefd14139452ae27fedb367
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-07-17 16:40:31 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+    appName: "Rongkhem Smart Village",
+
+    version: "1.0.0",
+
+    village: "บ้านร่องเข็ม หมู่ที่ 6",
+
+    province: "จังหวัดพะเยา",
+
+    dataPath: "data/",
+
+    imagePath: "images/",
+
+    apiVersion: "v1"
+
+};
