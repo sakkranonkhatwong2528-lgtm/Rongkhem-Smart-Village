e1c9aa57b61ef0ec27d4bd4dd15703d88f3f04f8
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-07-18 15:36:20 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "rongkhem-v2";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/css/style.css",
+"/js/app.js"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache=>cache.addAll(urlsToCache))
+
+);
+
+});
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
