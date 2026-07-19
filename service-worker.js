32830398f1cbeee5fb08b9d71121ff7721d81697
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-07-19 21:54:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,93 @@
-const CACHE_NAME = "rongkhem-v2";
+const CACHE_NAME =
+"rongkhem-smart-village-v1";
 
-const urlsToCache = [
-"/",
-"/index.html",
-"/css/style.css",
-"/js/app.js"
+
+const FILES = [
+
+
+"index.html",
+
+"dashboard.html",
+
+"css/style.css",
+
+"css/dashboard.css",
+
+"js/dashboard.js",
+
+"data/citizen.json",
+
+"data/household.json",
+
+"data/vulnerable.json"
+
 ];
 
-self.addEventListener("install", event => {
+
+
+
+
+self.addEventListener(
+"install",
+
+event=>{
+
 
 event.waitUntil(
 
-caches.open(CACHE_NAME)
+caches.open(
+CACHE_NAME
+)
 
-.then(cache=>cache.addAll(urlsToCache))
+.then(cache=>{
 
+return cache.addAll(
+FILES
 );
-
-});
-
-self.addEventListener("fetch",event=>{
-
-event.respondWith(
-
-caches.match(event.request)
-
-.then(response=>{
-
-return response || fetch(event.request);
 
 })
 
 );
 
-});
+
+}
+
+);
+
+
+
+
+
+
+
+self.addEventListener(
+"fetch",
+
+event=>{
+
+
+event.respondWith(
+
+caches.match(
+event.request
+)
+
+.then(response=>{
+
+
+return response ||
+
+fetch(
+event.request
+);
+
+
+})
+
+
+);
+
+
+}
+
+);
